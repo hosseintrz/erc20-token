321bbdc91c4b0975d777b225f017330830646d40
hosseintrz <htrzit@gmail.com>
2022-12-25 22:41:29 +0330

    erc20 token impl

--- erc20.sol
+++ erc20.sol
@@ -1,1 +1,104 @@
 pragma solidity ^0.8.0;
+
+contract SafeMath {
+ 
+    function safeAdd(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+ 
+    function safeSub(uint a, uint b) internal pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+ 
+    function safeMul(uint a, uint b) internal pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+ 
+    function safeDiv(uint a, uint b) internal pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+ 
+interface ERC20Interface {
+
+    function totalSupply() external view returns (uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function transfer(address to, uint tokens) external returns (bool success);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+ 
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract HTCToken is ERC20Interface, SafeMath {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
+
+    uint bricks;
+    uint blocks;
+    uint stones;
+    uint cement;
+
+    uint required_bricks;
+    uint required_blocks;
+    uint required_stones;
+    uint required_cement;
+    uint token_gen_rate;
+ 
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+ 
+    constructor() public {
+        symbol = "HTC";
+        name = "HOSSEINTRZ Coin";
+        decimals = 10;
+        _totalSupply = 1000000000000;
+        balances[msg.sender] = _totalSupply;
+    }
+ 
+    function totalSupply() public override view returns (uint) {
+        return _totalSupply;
+    }
+ 
+    function balanceOf(address tokenOwner) public override view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+ 
+    function transfer(address to, uint tokens) public override returns (bool success) {
+        require(balances[msg.sender] >= tokens, "not enough balance");
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+ 
+    function approve(address spender, uint tokens) public override returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+ 
+    function transferFrom(address from, address to, uint tokens) public override returns (bool success) {
+        require(tokens <= balances[from], "not enough balance");
+        require(tokens <= allowed[from][msg.sender], "you can't access this amount of tokens");
+        
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+ 
+    function allowance(address tokenOwner, address spender) public override view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+}
+
